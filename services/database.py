3ccf20b1b82f3ec4d1b1ac7187f0a71c51b4c60b
Metsyu <jlmaldon@uark.edu>
2022-09-26 20:40:13 -0500

    user_exist implemented

--- services/database.py
+++ services/database.py
@@ -1,8 +1,15 @@
-#class containing helper method for interacting with player database
+# Class containing helper method for interacting with player database
+
 
 def user_exists(id):
-    print('return if the user with the give id exists')
-    return False
+    conn = psycopg2.connect("dbname=something user=someone")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM player WHERE id = %s;", (id))
+    if cur.fetchone():
+        return True
+    else:
+        return False
+
 
 def get_user(id):
     print('return the name fo the user with the given id in the database')
