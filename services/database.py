7810d2a225b034113c7191be583cc3023acc8002
Metsyu <jlmaldon@uark.edu>
2022-09-27 14:32:38 -0500

    Implemented add_user() and del_user(). Implemented basic error checking for all methods.

--- services/database.py
+++ services/database.py
@@ -1,4 +1,4 @@
-# Class containing helper method for interacting with player database
+# Class containing helper methods for interacting with player database
 from sql_database import database_connection
 
 # Global variables
@@ -17,8 +17,31 @@ def user_exists(id):
 
 
 def get_user(id):
-    cur.execute("SELECT * FROM player WHERE id = %s", (id,))
-    data = cur.fetchone()
+    # If user exists, return the user's codename
+    if user_exists(id):
+        cur.execute("SELECT * FROM player WHERE id = %s;", (id,))
+        data = cur.fetchone()
 
-    # Return codename
-    return data[3]
+        return data[3]
+    # Else, return error statement
+    else:
+        return "ERROR: User does not exist!"
+
+
+def add_user(id, codename):
+    # If user already exists in the table, print error statement
+    if user_exists(id):
+        print("ERROR: User already exists!")
+    # Else, add the player to the table
+    else:
+        cur.execute("INSERT INTO player (id, first_name, last_name, codename) VALUES (%s, %s, %s, %s);",
+                (id, None, None, codename))
+
+
+def del_user(id):
+    # If user exists in the table, delete them from the table
+    if user_exists(id):
+        cur.execute("DELETE FROM player WHERE id = %s;", (id,))
+    # Else, print error statement
+    else:
+        print("ERROR: User does not exist!")
