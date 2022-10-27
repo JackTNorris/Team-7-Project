6e571f798db568ecd66dff26e51c35bdf52545ce
Mitchell Bylak <mjbylak@uark.edu>
2022-10-27 16:17:32 -0500

    first iteration (needs cleaning)

--- countdown_timer_screen.py
+++ countdown_timer_screen.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import tkinter as tk
+import time
 from tkinter import *
 from tokenize import Double
 
@@ -17,19 +18,26 @@ def countdown_timer_screen():
     width =   1280 #int(window.winfo_width()) #
     height =  720 #int(window.winfo_height()) #
 
-    #Creates a Tkinter-compatible photo image, which can be used everywhere Tkinter expects an image object.
-    img = ImageTk.PhotoImage(Image.open("splash_screen.jpeg").resize((width, height)))
-    #The Label widget is a standard Tkinter widget used to display a text or image on the screen.
-    panel = tk.Label(window, image = img)
-
-    #The Pack geometry manager packs widgets in rows or columns.
-    panel.pack(side = "bottom", fill = "both", expand = "yes")
+    #Configure the background
+    window.config(bg='burlywood1')
+    #Create Entry Widgets for SS
+    seconds = StringVar()
+    Entry(window, textvariable=seconds, width = 2, font = 'Helvetica 14').place(x=220, y=120)
+    seconds.set('30')
 
     #Start the GUI
-    window.after(3000, window.destroy)
+    
+    times = 30
+    while times > -1:
+        #Update the time
+        window.update()
+        time.sleep(1)
+        times -= 1
+        seconds.set(times)
 
+    Label(window, font =('Helvetica bold',22), text = 'Set the Timer',bg ='burlywood1').place(x=105,y=70)
+    Button(window, text='START', bd ='2', bg = 'IndianRed1',font =('Helveticabold',10), command = countdowntimer).place(x=167, y=165)
     window.mainloop()
-
 
 if __name__ == '__main__':
     countdown_timer_screen()
