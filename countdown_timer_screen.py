826d7c3eb569cb1a5892b7c4012bab40ed314bec
Jack Norris <jacktimothynorris@gmail.com>
2022-11-03 14:11:29 -0500

    finished countdown timer

--- countdown_timer_screen.py
+++ countdown_timer_screen.py
@@ -12,31 +12,25 @@ def countdown_timer_screen():
     window.title("Join")
     window.geometry("1280x720")
     window.configure(background='grey')
-
-    path = "Aaron.jpg"
-    # window.state('zoomed')
-    width =   1280 #int(window.winfo_width()) #
-    height =  720 #int(window.winfo_height()) #
+    times = 30
 
     #Configure the background
-    window.config(bg='burlywood1')
-    #Create Entry Widgets for SS
-    seconds = StringVar()
-    Entry(window, textvariable=seconds, width = 2, font = 'Helvetica 14').place(x=220, y=120)
-    seconds.set('30')
+    window.config(bg='black')
 
-    #Start the GUI
+    #Create widgets for timer
+    seconds = StringVar()
+    Label(window, textvariable=seconds, width = 2, font = 'Helvetica 50', bg='black', fg="red").place(x=600, y=300)
+    seconds.set(times)
     
-    times = 30
-    while times > -1:
+    while times > -1 and window.winfo_exists():
         #Update the time
         window.update()
         time.sleep(1)
         times -= 1
         seconds.set(times)
 
-    Label(window, font =('Helvetica bold',22), text = 'Set the Timer',bg ='burlywood1').place(x=105,y=70)
-    Button(window, text='START', bd ='2', bg = 'IndianRed1',font =('Helveticabold',10), command = countdowntimer).place(x=167, y=165)
+    if times < 0:
+        window.destroy()
     window.mainloop()
 
 if __name__ == '__main__':
