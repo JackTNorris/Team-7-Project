2903777d588cba02e8e06e83af68f6656862b4f6
Mitchell Bylak <mjbylak@uark.edu>
2022-11-17 15:16:17 -0600

    sharing-to-joey
    Co-authored-by: Joseph Maldonado <jlmaldon@uark.edu>

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
+import queue
 from tkinter import Tk
 from screens.player_input_screen import player_input_screen
 from splash_screen import splash_screen
 from countdown_timer_screen import countdown_timer_screen
 from player_action_screen import player_action_screen
-from udp_listen import create_thread
+from udp_thread import create_thread
 
 #this will be the entry point of our application
 
@@ -13,8 +14,18 @@ splash_screen()
 players = player_input_screen()
 countdown_timer_screen()
 
+q = queue.Queue()
+create_thread(q)
 player_action_screen(players)
 #tempororary for hector and gideon
 
-
 print(players)
+
+# main
+# C:/Users/mitch/AppData/Local/Programs/Python/Python310/python.exe "c:/Users/mitch/Documents/College/Fall 2022/Software Engineering/Project/Team-7-Project/main.py"
+# server
+# C:/Users/mitch/AppData/Local/Programs/Python/Python310/python.exe "c:/Users/mitch/Documents/College/Fall 2022/Software Engineering/Project/Team-7-Project/misc/udp_serv.py"
+# client
+# C:/Users/mitch/AppData/Local/Programs/Python/Python310/python.exe "c:/Users/mitch/Documents/College/Fall 2022/Software Engineering/Project/Team-7-Project/misc/udp_cli.py"
+# # traffic test
+# C:/Users/mitch/AppData/Local/Programs/Python/Python310/python.exe "c:/Users/mitch/Documents/College/Fall 2022/Software Engineering/Project/Team-7-Project/misc/udp_traffictest.py"
--- misc/udp_cli.py
+++ misc/udp_cli.py
@@ -5,20 +5,21 @@ def read_socket():
 	msgFromServer = UDPClientSocket.recvfrom(bufferSize) 
 	msg = msgFromServer[0]
 
-	msg = "Message from Server {}".format(msgFromServer[0])
-	print(msg)
+	#msg = "Message from Server {}".format(msgFromServer[0])
+	# # print(msg)
+	# print("i hate joey")
 	
 	return msg
 
 	
-if __name__ == '__main__':
-	msgFromClient       = "Hello UDP Server"
-	bytesToSend         = str.encode(msgFromClient)
-	serverAddressPort   = ("127.0.0.1", 7500)
-	bufferSize          = 1024
+#if __name__ == '__main__':
+msgFromClient       = "Hello UDP Server"
+bytesToSend         = str.encode(msgFromClient)
+serverAddressPort   = ("127.0.0.1", 7500)
+bufferSize          = 1024
 
-	# Create a UDP socket at client side
-	UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+# Create a UDP socket at client side
+UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 
-	# Send to server using created UDP socket
-	UDPClientSocket.sendto(bytesToSend, serverAddressPort)
+# Send to server using created UDP socket
+UDPClientSocket.sendto(bytesToSend, serverAddressPort)
